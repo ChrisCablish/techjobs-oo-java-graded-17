cffd095f8856f75fe6339a5b9cc0520ae991a7a5
ChrisCablish <112437010+ChrisCablish@users.noreply.github.com>
2023-09-19 12:20:32 -0500

    DRY code by utilizing inheritance

--- src/main/java/org/launchcode/techjobs/oo/test/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobField.java
@@ -1,4 +1,61 @@
 package org.launchcode.techjobs.oo.test;
 
+import java.util.Objects;
+
 public class JobField {
+
+    //fields
+    int id;
+    int nextId;
+    String value;
+
+    //constructors
+    public JobField () {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField (String value) {
+        this();
+        this.value = value;
+    }
+
+
+    //getters/setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
+    //custom methods
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Location)) return false;
+        JobField jobField = (JobField) o;
+        return getId() == jobField.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+
+
 }
